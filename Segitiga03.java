46c2547ab2d7260951ac72e59eb399c2e8e93155
AlexanderDev2004 <alexgaming2120@gmail.com>
2023-09-14 15:41:54 +0700

    New Joobshit Daspro

---
+++ Segitiga03.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Segitiga03 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int alas, tinggi;
+        float luas;
+        System.out.println("masukan Alas : ");
+        alas = sc.nextInt();
+        System.out.println("Masukan Tinggi : ");
+        tinggi =sc.nextInt();
+        luas = alas * tinggi / 2;
+        System.out.println("luas Segitiga : " + luas);
+
+    }
+}
